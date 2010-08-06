e73217e7fecd30381468695df09b5852ac2f29a2
Spavacz <spav@spav.com.pl>
2010-08-06 02:30:54 +0200

    Poprawka w _d()

--- cms/Bootstrap.php
+++ cms/Bootstrap.php
@@ -82,6 +82,7 @@ function _d($data)
 		$out = preg_replace('/([ \t]*)(\[[^\]]+\][ \t]*\=\>[ \t]*[a-z0-9 \t_]+)\n[ \t]*\(/iUe',"'\\1<a href=\"javascript:toggleDisplay(\''.(\$id = substr(md5(rand().'\\0'), 0, 7)).'\');\">\\2</a><div id=\"'.\$id.'\" style=\"display: none;\">'", $out);
 		// replace ')' on its own on a new line (surrounded by whitespace is ok) with '</div>
 		$out = preg_replace('/^\s*\)\s*$/m', '</div>', $out);
+		$out = preg_replace('/<\/div>$/', '', $out);
 		// print the javascript function toggleDisplay() and then the transformed output
 		echo '<script language="Javascript">function toggleDisplay(id) { document.getElementById(id).style.display = (document.getElementById(id).style.display == "block") ? "none" : "block"; }</script>'."\n<pre style=\"background-color: white;text-align:left;\">$out</pre>";
 	}
