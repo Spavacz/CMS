31e04aa0f932c02cc7a92adbf8ebec8f34ac41cd
Spavacz <spav@spav.com.pl>
2010-11-06 01:05:51 +0100

    Nawigacja w panelu administracyjnym

--- cms/modules/cms/views/helpers/AdminBreadcrumbs.php
+++ cms/modules/cms/views/helpers/AdminBreadcrumbs.php
@@ -3,7 +3,14 @@
 class Zend_View_Helper_AdminBreadcrumbs extends Zend_View_Helper_Abstract
 {
     public function adminBreadcrumbs()
-    {
-		return $this->view->partial('helpers/AdminBreadcrumbs.phtml');
+    {	
+    	$config = new Zend_Config_Xml(APPLICATION_PATH . '/configs/admin_pages.xml');
+    	$this->view->navigation(new Zend_Navigation($config));
+    	
+    	$this->view->navigation()->breadcrumbs()
+    		->setMinDepth()
+    		->setSeparator('</li><li>')
+    		->setPartial('helpers/AdminBreadCrumbs.phtml');
+		return $this->view->navigation()->breadcrumbs()->render();
     }
 }
--- cms/modules/cms/views/helpers/AdminMenu.php
+++ cms/modules/cms/views/helpers/AdminMenu.php
@@ -7,6 +7,13 @@ class Zend_View_Helper_AdminMenu extends Zend_View_Helper_Abstract
 		if(!Zend_Auth::getInstance()->hasIdentity()) {
 			return '';
 		}
-		return $this->view->partial('helpers/AdminMenu.phtml');
+		
+		$config = new Zend_Config_Xml(APPLICATION_PATH . '/configs/admin_menu.xml');
+    	$this->view->navigation(new Zend_Navigation($config));
+    	
+    	$menu = $this->view->navigation()->menu()
+			->setUlClass('sf-menu sf-navbar')
+			->render();
+		return '<div id="nav">' . $menu . '</div>';
     }
 }
